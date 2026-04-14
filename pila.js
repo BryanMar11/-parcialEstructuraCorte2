23d5be0209ccfb86d2185493b04c69aae78cb010
BryanMar11 <ebmpepe22@gmail.com>
2026-04-13 20:51:31 -0500

    Arreglo logica pila

--- pila.js
+++ pila.js
@@ -8,26 +8,27 @@ const rl = readline.createInterface({
 let pila = [];
 
 function menu() {
-  console.log("\nPila actual:", pila);
-  rl.question("¿Qué deseas hacer? (1: Agregar, 2: Eliminar, 3: Salir): ", (opcion) => {
+  console.log(pila);
+  
+  rl.question("1. Agregar Objeto\n2. Eliminar Objeto\n3. Salir\n> ", (opcion) => {
     if (opcion === "1") {
-      rl.question("Digita el número a agregar: ", (numero) => {
-        pila.push(Number(numero));
-        console.log("Agregado:", numero);
-        menu();
+      rl.question("Propiedad 1: ", (p1) => {
+        rl.question("Propiedad 2: ", (p2) => {
+          pila.push({ dato1: p1, dato2: p2 });
+          console.log("Agregado.");
+          menu();
+        });
       });
     } else if (opcion === "2") {
       if (pila.length > 0) {
-        let eliminado = pila.pop();
-        console.log("Eliminado:", eliminado);
+        console.log("Eliminado:", pila.pop());
       } else {
-        console.log("La pila está vacía.");
+        console.log("Vacía.");
       }
       menu();
     } else if (opcion === "3") {
       rl.close();
     } else {
-      console.log("Opción no válida.");
       menu();
     }
   });
