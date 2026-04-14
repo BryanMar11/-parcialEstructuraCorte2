3cd00c3f89114ebc7ca0f4fe8ff00adb2be86143
brahianmartinez01 <brahian.martinez01@unicatolica.edu.co>
2026-04-13 19:24:07 -0500

    Se agrega la logica de la pila

---
+++ pila.js
@@ -0,0 +1,12 @@
+let pila = [];
+
+let objeto_1 = { id: 1 };
+let objeto_2 = { id: 2 };
+
+pila.push(objeto_1);
+pila.push(objeto_2);
+
+let_eliminado = pila.pop();
+
+console.log("Objeto eliminado:", eliminado);
+console.log("Pila actual:", pila);
